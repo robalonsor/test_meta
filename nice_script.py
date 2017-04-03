0958001b9225721d00eb7f25b394bdbc04cacf5e
Roberto Alonso <zerox058017@hotmail.com>
2017-04-03 16:45:03 +0200

    fourht comment!

--- nice_script.py
+++ nice_script.py
@@ -48,3 +48,48 @@ class GraphFileReader(object):
 
         print("Successfully constructed vertex set")
         print(self.vertex_type_dic)
+    # print("Constructing edge set")
+
+        # G = nx.read_graphml(f.name, node_type=int)  # Constr. edge set using networkx+graphml reader
+        # print(nx.to_dict_of_lists(G))
+        # self.edge_set = nx.to_dict_of_lists(G)
+        # self.graph_construction()
+    #
+    # def graph_construction(self):
+    #     V_type = self.vertex_type_dic  # vertex type
+    #     E = self.edge_set  # set of edges
+    #     g = self.graph
+    #     for key_v_id in E:
+    #         edges_to = E[key_v_id]  # the list of vertices with edge to <key_v_id> vertex
+    #         v1 = Vertex(key_v_id, V_type[key_v_id])
+    #         for v in edges_to:
+    #             v2 = Vertex(v, V_type[v])
+    #             e = Edge(v1, v2, 100)  # 100 is an arbitrary value
+    #             v1.add_edge(0, e)
+    #         g.add_vertex(v1)
+    #     self.graph = g
+    #     if g:
+    #         return True
+    #     return
+
+        # v1 = Vertex(1, "A")
+        # v2 = Vertex(2, "B")
+        # e = Edge(v1, v2, 100)
+        #
+        # v1.add_edge(0,e)
+        # v2.add_edge(0,e)
+        # g1.add_vertex(v1)
+        # g1.add_vertex(v2)
+
+
+
+
+# g_reader = GraphFileReader("datasets/bipartite.graphml")
+# g_reader.generate_graph()
+# g = g_reader.graph
+# #
+# G = nx.Graph(g.to_dict_of_lists(0))
+#
+# import matplotlib.pyplot as plt
+# nx.draw(G, pos=nx.spring_layout(G), with_labels=True)
+# plt.show()
