ee422345fe867d85c247ccf7b4db9ae35e48beff
Roberto Alonso <zerox058017@hotmail.com>
2017-04-03 16:44:45 +0200

    adding a third comment

--- nice_script.py
+++ nice_script.py
@@ -20,3 +20,31 @@ class GraphFileReader(object):
                 print("Starting to parse Graphml file")
                 f.close()
                 self.construct_graph_from_file(file_name)
+    def construct_graph_from_file(self, file_name):
+        f = open(file_name)
+        self.vertex_type_dic = {}
+        try:
+            for line in f:
+                if "<edge source=" in line:
+                    break
+                if "<node id=" in line:
+                    if "type" not in line:
+                        raise Exception("Error! Found vertex without a type. Exit")
+                    line = line[10:]
+                    stop = line.index('"')
+                    v_id = int(line[0:stop])
+                    line = line[stop+8:]
+                    stop = line.index('"')
+                    v_type = line[0:stop]
+                    if v_id in self.vertex_type_dic:
+                        raise Exception("Error! Possible duplicate vertex. Exit")
+                    self.vertex_type_dic[v_id] = v_type
+
+            f.close()
+            assert len(self.vertex_type_dic) > 1
+        except Exception as er:
+            print(er)
+            return
+
+        print("Successfully constructed vertex set")
+        print(self.vertex_type_dic)
