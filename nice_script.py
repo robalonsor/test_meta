dfa93af50494624a90ae2fc5c17327fb23bbac1a
Roberto Alonso <zerox058017@hotmail.com>
2017-04-03 16:44:31 +0200

    Adding a second comment

--- nice_script.py
+++ nice_script.py
@@ -9,3 +9,14 @@ class GraphFileReader(object):
         # self.edge_set = {}
         self.vertex_type_dic = {}
         # self.graph = Graph(2)
+    def generate_graph(self):
+            file_name = self.file_name
+            try:
+                f = open(file_name, 'r')
+            except IOError:
+                print('cannot open', file_name)
+            else:
+                print(file_name, 'has', len(f.readlines()), 'lines')
+                print("Starting to parse Graphml file")
+                f.close()
+                self.construct_graph_from_file(file_name)
